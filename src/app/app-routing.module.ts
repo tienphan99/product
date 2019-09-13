11ce62572cbbf5acbdd111d38580b6ad4d0fa231
tienphan99 <phanvantien488@gmail.com>
2019-09-13 17:29:02 +0700

    fix route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,8 +5,6 @@ import { DetailproductComponent } from './Chuong I/detailproduct/detailproduct.c
 import { Bai1Component } from './Chuong I/bai1/bai1.component';
 import { Bai2Component } from './Chuong I/bai2/bai2.component';
 import { Bai3Component } from './Chuong I/bai3/bai3.component';
-import { BAI3Service } from './Chuong I/service/BAI3.service';
-import { CartService } from './Chuong I/service/cart.service';
 import { GioHangComponent } from './Chuong I/giohang/giohang.component';
 import { NavBtComponent } from './Chuong I/nav-bt/nav-bt.component';
 import { SinhvienComponent } from './Chuong2/sinhvien/sinhvien.component';
@@ -17,11 +15,10 @@ const routes: Routes = [
   { path: 'Chuong I/bai1', component: Bai1Component},
   { path: 'Chuong I/bai2', component: Bai2Component},
   { path: 'Chuong I/bai3', component: Bai3Component},
-  { path: 'Chuong I/bai3service', component: BAI3Service},
-  { path: 'Chuong I/cart', component: CartService},
+ 
   { path: 'Chuong I/giohang', component: GioHangComponent},
   { path: 'Chuong I/productlist', component: ProductlistComponent},
-  { path: 'Chuong I/bai3', component: BAI3Service},
+ 
   { path: 'products/:productId', component: DetailproductComponent },
   { path: 'Chuong2', component: SinhvienComponent},
   { path: 'Chuong2/student/:id', component: DetailComponent},
